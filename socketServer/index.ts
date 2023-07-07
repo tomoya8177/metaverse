614d154976f0952ee3fb90fa617969b3b28cb260
tomoya8177 <imai@narra.jp>
2023-07-08 01:26:47 +0900

    Update index.ts

--- socketServer/index.ts
+++ socketServer/index.ts
@@ -10,7 +10,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://mymetaverseportal.net",
+    ],
   },
 });
 console.log({ io });
