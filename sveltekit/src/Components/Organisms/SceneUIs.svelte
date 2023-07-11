7d5fe4fc258c436f1099a7eefd97a1128b08c555
tomoya8177 <imai@narra.jp>
2023-07-11 21:21:41 +0900

    update from mac

--- sveltekit/src/Components/Organisms/SceneUIs.svelte
+++ sveltekit/src/Components/Organisms/SceneUIs.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import ChatBox from './ChatBox.svelte';
+
 	import TextChatMessage from '../Molecules/TextChatMessage.svelte';
 
 	import 'aframe';
@@ -18,10 +20,6 @@
 	import type { User } from '$lib/types/User';
 	import { editableObject } from '$lib/Classes/EditableObject';
 	import type { Me } from '$lib/Classes/Me';
-	let messages: Message[] = [];
-	let newMessagePinned = false;
-	let newMessageBody = '';
-	let authors: User[] = [];
 	const scrolToBottom = (element: Element) => {
 		element.scrollTop = element.scrollHeight;
 	};
@@ -34,86 +32,27 @@
 			scrolToBottom(element);
 		}, 100);
 	};
-	const loadMessages = async (existings: Message[] = []) => {
-		messages = [
-			...(await axios
-				.get('/api/messages?event=' + $EventStore.id + '&pinned=1')
-				.then((res) => res.data)),
-			...existings
-		].filter((thing, index, self) => self.findIndex((t) => t.id === thing.id) === index);
-		console.log({ messages });
-		authors = await axios
-			.get(`/api/users?id=in:'${messages.map((m) => m.user).join("','")}'`)
-			.then((res) => res.data);
-	};
-	onMount(async () => {
-		//loadMessages();
-		//onKeyDown
-		document.onkeydown = (e) => {
-			//check if any input has the focus
 
-			//shift + enter will send message
-			if (document.activeElement?.tagName === 'TEXTAREA' && e.key === 'Enter' && e.shiftKey) {
-				onMessageSendClicked();
-				return;
-			}
-
-			if (
-				document.activeElement?.tagName === 'INPUT' ||
-				document.activeElement?.tagName === 'TEXTAREA' ||
-				document.activeElement?.tagName === 'SELECT'
-			)
-				return;
-			if (e.key === 't') {
-				onTextChatClicked();
-			}
-		};
-		videoChat.listenTo('textMessage', (data) => {
-			console.log('received textMessage', data);
-			messages = [...messages, data];
-			if (data.user !== $UserStore.id) {
-				Users.find(data.user)?.say(data.body);
-			}
-		});
-	});
-	onDestroy(() => {
-		document.onkeydown = null;
-		videoChat.dontListenTo('textMessage');
-	});
-
-	const onMessageSendClicked = async () => {
-		if (newMessageBody.trim() === '') return;
-		const newMessage = {
-			body: escapeHTML(newMessageBody),
-			user: $UserStore.id,
-			event: $EventStore.id,
-			pinned: newMessagePinned
-		};
-		const createdMessage = await axios.post('/api/messages', newMessage).then((res) => res.data);
-		messages = [...messages, createdMessage];
-		videoChat.sendMessage({ ...createdMessage, key: 'textMessage' });
-		newMessageBody = '';
-		setTimeout(() => {
-			const element = document.querySelector('.chat-box > div');
-			if (!element) return;
-			scrolToBottom(element);
-		}, 100);
-	};
-	let textChatOpen = false;
-	$: trigger(textChatOpen);
-	const trigger = (textChatOpen: boolean) => {
-		if (textChatOpen) {
-			//focus on the textarea
-			loadMessages(messages);
-			setTimeout(() => {
-				const element = document.getElementById('chat-textarea')?.querySelector('textarea');
-				if (!element) {
-					return;
-				}
-				element.focus();
-			}, 100);
+	const onKeyDown = (e: KeyboardEvent) => {
+		if (
+			document.activeElement?.tagName === 'INPUT' ||
+			document.activeElement?.tagName === 'TEXTAREA' ||
+			document.activeElement?.tagName === 'SELECT'
+		)
+			return;
+		if (e.key === 't') {
+			onTextChatClicked();
 		}
 	};
+	onMount(async () => {
+		document.addEventListener('keydown', onKeyDown);
+	});
+	onDestroy(() => {
+		document.removeEventListener('keydown', onKeyDown);
+	});
+
+	let textChatOpen = false;
+
 	let rotation = 0;
 	export let me: Me | null = null;
 </script>
@@ -216,37 +155,7 @@
 	{/if}
 </div>
 {#if textChatOpen}
-	<div class="chat-box">
-		{#if textChatOpen}
-			<div style="overflow-y:auto;max-height:calc(100vh - 23rem)">
-				{#each messages.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1)) as message}
-					<TextChatMessage
-						{message}
-						author={authors.find((a) => a.id === message.user) || null}
-						onDelete={(messageId) => {
-							messages = messages.filter((m) => m.id !== messageId);
-						}}
-					/>
-				{/each}
-			</div>
-			<hr />
-			<div style="text-align:right">
-				<InputWithLabel label="Pinned" type="switch" bind:value={newMessagePinned} />
-			</div>
-			<div style="display:flex;gap:0.4rem">
-				<div style="flex:1" id="chat-textarea">
-					<InputWithLabel label="" type="textarea" bind:value={newMessageBody} />
-				</div>
-				<div>
-					<button
-						data-tooltip="Shift + Enter"
-						style="margin-bottom:0rem"
-						on:click={onMessageSendClicked}>Send</button
-					>
-				</div>
-			</div>
-		{/if}
-	</div>
+	<ChatBox />
 {/if}
 <div id="cameraPreviews">
 	<video class="remoteVideo" style="display:none" />
