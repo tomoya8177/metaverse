505f85f31dc71e4cec8cf8025125f8bc11822458
tomoya8177 <imai@narra.jp>
2023-08-03 18:41:45 +0900

    euler bug fix

--- sveltekit/src/lib/frontend/getPositionFromLockedPosition.ts
+++ sveltekit/src/lib/frontend/getPositionFromLockedPosition.ts
@@ -4,6 +4,6 @@ import type { xyz } from '$lib/store';
 export const getPositionFromLockedPosition = (lockedPosition: number): xyz => {
 	const angle = lockedPosition * 30;
 	const offsetVector = new THREE.Vector3(0, 1.65, -5);
-	offsetVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), degree2radian(angle));
+	offsetVector.applyAxisAngle(new THREE.Vector3(0.001, 1, 0.001), degree2radian(angle));
 	return { x: offsetVector.x, y: offsetVector.y, z: offsetVector.z };
 };
