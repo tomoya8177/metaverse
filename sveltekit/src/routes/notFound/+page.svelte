2b91985c3eaec80b10a86f4c1ea3e242c075d26f
tomoya8177 <imai@narra.jp>
2023-08-14 12:16:28 +0900

    redirect to not found page when url incorrect

---
+++ sveltekit/src/routes/notFound/+page.svelte
@@ -0,0 +1,14 @@
+<div>
+	PAGE NOT FOUND<br />
+	Please make sure you have the correct URL.
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		font-size: 2rem;
+	}
+</style>
