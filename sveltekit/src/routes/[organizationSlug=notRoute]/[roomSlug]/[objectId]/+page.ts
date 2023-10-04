9f0b817038cf8848a1e686dde078453ec1488d56
tomoya8177 <imai@narra.jp>
2023-10-04 23:01:01 +0900

    apiCall now returns the actua value

--- sveltekit/src/routes/[organizationSlug=notRoute]/[roomSlug]/[objectId]/+page.ts
+++ sveltekit/src/routes/[organizationSlug=notRoute]/[roomSlug]/[objectId]/+page.ts
@@ -3,11 +3,9 @@ import axios from 'axios';
 
 export const load = async ({ params }) => {
 	let attendances = [];
-	const event =
-		(await apiCall.getOne('/api/events?object=' + params.objectId).then((res) => res.data[0])) ||
-		null;
+	const event = (await apiCall.getOne('/api/events?object=' + params.objectId)) || null;
 	if (event) {
-		attendances = await apiCall.get('/api/attendances?event=' + event.id).then((res) => res.data);
+		attendances = await apiCall.get('/api/attendances?event=' + event.id);
 	}
 	return {
 		event,
