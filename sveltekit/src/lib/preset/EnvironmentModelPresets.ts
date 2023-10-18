103e1bd2e41bd664de6256ef89003907bcb90b8f
tomoya8177 <imai@narra.jp>
2023-10-18 17:15:09 +0900

    updatedMesh

--- sveltekit/src/lib/preset/EnvironmentModelPresets.ts
+++ sveltekit/src/lib/preset/EnvironmentModelPresets.ts
@@ -65,11 +65,11 @@ export const EnvironmentModelPresets: Environment[] = [
 	{
 		name: _('Conference Room') + '1',
 		modelURL: '/models/Room1.glb',
-		navMeshURL: '/models/navmesh2.glb'
+		navMeshURL: '/models/navmesh1.glb'
 	},
 	{
 		name: _('Conference Room') + '2',
 		modelURL: '/models/Room2.glb',
-		navMeshURL: '/models/navmesh1.glb'
+		navMeshURL: '/models/navmesh2.glb'
 	}
 ];
